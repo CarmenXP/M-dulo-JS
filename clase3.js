16efdd3dc82e31b7c37f496c40cec70d0dcad29f
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-09-26 00:37:42 -0500

    clase 4

---
+++ clase3.js
@@ -0,0 +1,82 @@
+//function nacimiento(){
+//    nombre= prompt("¿Cuál es tu nombre?")
+//    edad= Number(prompt("¿Cuál es tu edad"))
+//    año= 2020-edad;
+//
+//    return `  Hola ${nombre} tu año de nacimiento es ${año}`
+//
+//}
+//console.log(nacimiento())
+
+
+//function comparacion(){
+//    num1= Number(prompt("Ingresa un número"))
+//    num2= Number(prompt("Ingresa otro número"))
+//
+//    if (num1 > num2) {
+//     console.log ( num1 + "es mayor")
+//    } 
+//    else if (num1 < num2){
+//    console.log ( num1 + "es menor")
+//
+//    }
+//     else {
+//        console.log ( " son iguales")
+//    }
+//
+//
+//}
+//
+//comparacion()
+
+
+//var  queHaces = function(trabajo, nombre){
+//    switch(trabajo){
+//        case 'programador':
+//            return nombre + " Debes tener mucha paciencia";
+//        case 'diseñador':
+//            return nombre + "Debes de ser muy creativo";
+//        default:
+//            return nombre +  " No sé que es eso, pero cuentame más"        
+//    }
+//
+//}
+//
+//console.log(queHaces('programador', 'Car'))
+
+//(function calcularEdad(nacimiento, nombre){
+//    let edad= 2020-nacimiento;
+//    let infoUsuario= edad + nombre;
+//    console.log(infoUsuario);
+//    return infoUsuario;
+//}(1987, "  car"));
+
+
+//function exponent(base, exponent){
+//    let resultado = 1;
+//        for (let i=1; i<=exponent; exponent++){
+//          resultado *= base
+//          console.log(resultado)
+//          
+//        }
+//        return resultado
+//        
+//        
+//}
+//
+////exponent(5, 2)
+//
+//function power(base, exponent) {
+//  var result = 1;
+//
+//  for(var i = 0; i < exponent; i++) {
+//    result *= base; // Same as: result = result * base;
+//  }
+//
+//  return result;
+//}
+//
+//
+//power(5, 2)
+
+
---
+++ clase4.js
@@ -0,0 +1,71 @@
+// Dada la serie del 1 al 100, crear una función que imprime Fizz en lugar de números divisibles entre 3 y 
+// buzz de los números divisibles entre 5, FizzBuzz si son divisibles entre 3 y 5
+
+//for (let i=1; i <= 100; i++){
+//    if( i % 3 ===0 && i % 5 ===0){
+//        console.log("FizzBuzz")
+//    } else if(i % 3 === 0){
+//        console.log("fizz")
+//    } else if(i % 5 === 0){
+//        console.log("buzz")
+//    }else{
+//        console.log(i)
+//    }
+//}
+
+/*
+  Calcula el BURP
+
+  Crear una función llamada calcula_burp que recibe como 
+  parámetro un número entero positivo y debe regresar
+  una cadena de texto que sea BURP con tantas R como 
+  el parámetro.
+
+  calcula_burp(5); // "BURRRRRP"
+  calcula_burp(10); // "BURRRRRRRRRRP";
+*/
+
+
+//function burp(n){
+//    let add = "";
+//    for(let i=1; i <=n; i++){
+//        add+= "R";
+//    }
+//    return "BU"+ add + "P"
+//}
+//
+//console.log(burp(3))
+
+/*
+Crear una función que sume todos los elementos de un array
+ */
+
+ //
+ //function sumaArray(arr){
+ //    let sum= 0;
+ //    for(let i=0; i< arr.length; i++){
+ //        sum+= arr[i]
+ //    }
+ //    return sum
+ //}
+//
+ //console.log(sumaArray([3,6,8,9,1,0]))
+
+ /*
+ Crear una función  a la cual al pasarle un array y un número , devuelva true si el elemento pertenece al arra, 
+ caso contario devuelve false
+  */
+ //let arr= [3,6,8,9,1,0]
+//
+ // function include(arr, element){
+ //     for(let i=0; i< arr.length; i++){
+ //         if(arr[i] === element){
+ //             return true
+ //         }
+ //         
+ //     }
+ //     return false     
+ // }
+//
+ // console.log(include(arr, 3))
+ 
